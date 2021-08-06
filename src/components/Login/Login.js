d98964c5df05d03a27e891cde0265cf548615232
Aditi404 <78465360+Aditi404@users.noreply.github.com>
2021-08-06 18:37:18 +0530

    updated upto 56 min

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,64 +1,83 @@
 import { Button, TextField } from "@material-ui/core";
 import React, { useState } from "react";
 import { auth } from "../../lib/firebase";
+import Signup from "../Signup/Signup";
 import "./styles.css";
 
 const Login = () => {
     const [loading, setLoading] = useState(false);
+    const [showSignUp, setShowSignUp] = useState(false);
+    const toogleSignUp = ()=>{
+      setLoading(true);
+      //w8 for few seconds
+      setTimeout(() => {
+        setLoading(false);
+      setShowSignUp(true);
+
+        
+      },1500);
+      
+    }
+
+
     return (
-        <div className="login background--white">
-        <div className="login__content">
-        {loading && <div className="login__loading" />}
-          <div className="login__wrapper">
-            <img
-              className="login__logo"
-              src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-              alt="Google"
-            />
-
-            <p className="login__title">Sign in</p>
-            <p className="login__subtitle">Continue to Gmail</p>
-
-            <form className="login__form">
-              <TextField
-                id="outlined-basic"
-                label="Email"
-                variant="outlined"
-                className="login__input"
-                type="email"
+        <div className="login ">
+          {showSignUp ?(
+            <Signup setShowSignUp={setShowSignUp}/>)
+          :(  <div className="login__content">
+          {loading && <div className="login__loading" />}
+            <div className={`login__wrapper ${loading && "login__fade"}`}>
+              <img
+                className="login__logo"
+                src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+                alt="Google"
               />
-
-              <TextField
-                id="outlined-basic"
-                label="Password"
-                variant="outlined"
-                className="login__input"
-                type="password"
-              />
-              <div className="login__infoText">
-                Not your computer? Use guest mode to sign in privately?
-                <a href="/learnmore">Learn More</a>
-              </div>
-
-              <div className="login__buttons">
-                <Button
-                  className="login__button"
-                  color="primary"
-                >
-                  Create Account
-                </Button>
-
-                <Button
-                  className="login__button"
-                  color="primary"
-                  variant="contained"
-                >
-                  Sign in
-                </Button>
-              </div>
-            </form>
-          </div>
-        </div>
+  
+              <p className="login__title">Sign in</p>
+              <p className="login__subtitle">Continue to Gmail</p>
+  
+              <form className="login__form">
+                <TextField
+                  id="outlined-basic"
+                  label="Email"
+                  variant="outlined"
+                  className="login__input"
+                  type="email"
+                />
+  
+                <TextField
+                  id="outlined-basic"
+                  label="Password"
+                  variant="outlined"
+                  className="login__input"
+                  type="password"
+                />
+                <div className="login__infoText">
+                  Not your computer? Use guest mode to sign in privately?
+                  <a href="/learnmore">Learn More</a>
+                </div>
+  
+                <div className="login__buttons">
+                  <Button
+                    className="login__button"
+                    color="primary"
+                    onClick={toogleSignUp}
+                  >
+                    Create Account
+                  </Button>
+  
+                  <Button
+                    className="login__button"
+                    color="primary"
+                    variant="contained"
+                  >
+                    Sign in
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </div>)}
+      
     </div>
     )
 }
---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,176 @@
+import {  Button,
+    Checkbox,
+    FormControlLabel,
+    TextField, } from '@material-ui/core';
+import { FormatIndentDecreaseTwoTone } from '@material-ui/icons';
+
+import React from 'react';
+import  { useState } from "react";
+import { auth } from '../../lib/firebase';
+import './styles.css';
+const initialFormData={firstName: "", lastName: "", email: "", password: "",confirmPassword:"",}
+const Signup = ({setShowSignUp}) => {
+    const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({initialFormData});
+    const [checked, setChecked] = useState(false);
+    const [passwordError, setPasswordError]  = useState({state:false,msg:''});
+    const [emailError, setEmailError]  = useState({state:false,msg:''});
+    const toogleSignUp=()=>{
+        setLoading(true);
+        setTimeout(()=>{
+            setLoading(false);
+            setShowSignUp(false);
+
+        }, 1500)
+    }
+    const createAccount = (e) =>{
+        e.preventDefault();
+        setLoading(true);
+        const error = formData.password===formData.confirmPassword
+        if(!error){
+            setPasswordError({state:true,msg: "Passwords donot match"})
+            setFormData({ ...formData, confirmPassword: ""});
+            setLoading(false);
+            return;
+        } else{
+            setPasswordError({ state: false, msg: ""});
+            setEmailError({ state: false, msg: ""});
+        }
+        auth.createUserWithEmailAndPassword(formData.email, formData.password)
+        .then(() =>{
+            auth.currentUser.updateProfile({
+                displayName: '${formData.firstName} ${formData.lastName}',
+
+            }).then(()=>{
+                setLoading(false);
+                setEmailError({state: false, msg: ""});
+                setPasswordError({state: false, msg: ""});
+            })
+
+
+        })
+        // .catch((error) =>{
+        //     if(error.code === "")
+
+        // });
+
+    };
+    return (
+        <div>
+          
+            <div className="signup__container">
+                <div className={'signup $ {loading && "login__fade"}'}>
+                {loading && <div className="login__loading signup__loading" />}
+                    <div className="signup__container">
+                        <div className="signup__left">
+                            <img className="login__logo"
+                            src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+                            alt="Google"/>
+                            <h1 className="signup__heading">Create your Google Account</h1>
+                            <p className="signup__subheading">Continue to Gmail</p>
+                            <div className="signup__inputs">
+                                <div className="signup__nameInputs">
+                                    <TextField id="outlined-basic" label="First Name" type="name" className="signup__nameInput" variant="outlined"
+                                    value={FormData.firstName}
+                                    onChange={(e)=>{
+                                        setFormData({
+                                            ...formData,
+                                            firstName: e.target.value,
+                                        })
+                                    }}
+                                    />
+                                    <TextField id="outlined-basic" label="Last Name" type="name" className="signup__nameInput" variant="outlined"
+                                     value={FormData.lastName}
+                                     onChange={(e)=>{
+                                         setFormData({
+                                             ...formData,
+                                             lastName: e.target.value,
+                                         })
+                                     }}
+                                    />
+
+
+                                </div>
+                                <TextField id="outlined-basic" label=" Email" type="email" fullWidth  variant="outlined" helperText="You can use letters, numbers & periods"
+                                 value={FormData.email}
+                                 onChange={(e)=>{
+                                     setFormData({
+                                         ...formData,
+                                         email: e.target.value,
+                                     })
+                                 }}
+                                />
+                                <div className="signup__passwordInputs">
+                                    <div className="signup__passwordWrapper">
+                                    <TextField id="outlined-basic" label="Password" type={checked ? "text" : "password"} className="signup__nameInput" variant="outlined"
+                                    className="signup__passwordInput"
+                                    value={FormData.password}
+                                    onChange={(e)=>{
+                                        setFormData({
+                                            ...formData,
+                                            password: e.target.value,
+                                        })
+                                    }} />
+                                    <TextField id="outlined-basic" label="Confirm Password " type={checked ? "text" : "password"}  className="signup__nameInput" variant="outlined"
+                                     className="signup__passwordInput"
+                                     value={FormData.confirmPassword}
+                                     onChange={(e)=>{
+                                         setFormData({
+                                             ...formData,
+                                             confirmPassword: e.target.value,
+                                         })
+                                     }}
+                                     />
+                                    </div>
+                                    <p className="signup__helpertext">
+                                        Use 8 or more characters with mix of letters, numbers & symbols
+                                    </p>
+                                    <FormControlLabel
+                                    
+                                    control={
+                                        <Checkbox
+                                        checked={checked} 
+                                        color= "primary"
+                                        onClick={() => setChecked(!checked)}
+                                        />
+                                        
+                                    }
+                                    label="Show Password"
+                                    />
+
+                                </div>
+                                <div className="signup__buttons">
+                                    <Button className="signup__button" variant="text"
+                                    color="primary"
+                                    onClick={toogleSignUp}
+                                    >Sign in instead</Button>
+                                    <Button className="signup__button" variant="contained"
+                                    color="primary"
+                                    onClick={createAccount}
+                              
+                                    >Create</Button>
+                                </div>
+                            </div>
+
+                        </div>
+                        <figure className="signup__figure">
+                        <img 
+                        className="signup__figureImg"
+                        src="https://ssl.gstatic.com/accounts/signup/glif/account.svg"
+                        alt="account"
+                        />
+                        <figcaption className="signup___figcaption">
+                            One account. All of google working for you
+                        </figcaption>
+                    </figure>
+
+                    </div>
+                    
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Signup
